f72dd8a42305b8385d8095e22cbeeb54e85f710a
KhanhVu209 <154996858+KhanhVu209@users.noreply.github.com>
2024-01-30 23:50:56 +0700

    Delete dice_roll.py

--- dice_roll.py
+++
@@ -1,7 +0,0 @@
-import random
-from telegram import Update
-
-async def roll(update: Update, context) -> None:
-    # Tạo một số ngẫu nhiên từ 1 đến 100
-    random_number = random.randint(1, 100)
-    await update.message.reply_text(f'You rolled: {random_number}')
